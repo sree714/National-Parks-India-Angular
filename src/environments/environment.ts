3ff4e854d797663e0f26c7d8aa5d230e0eeba3fc
soumoq <soumojyotiganguly270@gmail.com>
2021-09-26 18:55:45 +0530

    Init

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "xAIzaSyAbnWnGY4DhuCned4nPTfo2JW7u15VJB2M",
+    authDomain: "angularfirebase-student-25334.firebaseapp.com",
+    databaseURL: "https://angularfirebase-student-25334-default-rtdb.firebaseio.com",
+    projectId: "angularfirebase-student-25334",
+    storageBucket: "angularfirebase-student-25334.appspot.com",
+    messagingSenderId: "881421570620",
+    appId: "1:881421570620:web:31d1f865d1875013f9a939"
+  },
+  googleMapsApi : "AIzaSyAbnWnGY4DhuCned4nPTfo2JW7u15VJB2M"
+};
